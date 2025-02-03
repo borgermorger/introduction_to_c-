55ab889d4b793e49fd49af39a283492a17c224de
Yeldos Baltabai <yeldosbaltabai@gmail.com>
2025-02-03 20:06:42 +0500

    second task of seminar c#_basis is solved

---
+++ c#_basis/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает целое число N, а на выходе показывает все целые числа промежутка -N до N.
+
+// Примеры: 4, 2
+
+
+using System;
+
+Console.WriteLine("Введите целое число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit < 0){
+    digit = -digit;
+}
+int count = -digit;
+while (count <= digit) {
+    Console.Write($"{count} ");
+    count++;
+}
