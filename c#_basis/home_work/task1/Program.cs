c27e01794e920afd87a611e708b90bafaf8375e1
Yeldos Baltabai <yeldosbaltabai@gmail.com>
2025-02-04 19:34:57 +0500

    solved first task of c#_basis homework

---
+++ c#_basis/home_work/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задание 1
+// Определите, делится ли число на другое
+// Описание: Напишите метод, который на вход принимает два целых числа и проверяет,
+// делится ли первое число на второе. Если делится, выводите "делится", иначе
+// выводите "не делится".
+// Пример использования: На входе:
+// ● firstNumber: 10
+// ● secondNumber: 2
+// На выходе:
+// ● делится
+// На входе:
+// ● firstNumber: 10
+// ● secondNumber: 3
+// На выходе:
+// ● не делится
+
+
+
+Console.Write("Введите первое целое число: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int second_num = Convert.ToInt32(Console.ReadLine());
+
+if (second_num == 0) {
+    Console.WriteLine("На ноль делить нельзя");
+}
+else if (first_num % second_num == 0) {
+    Console.WriteLine("делится");
+}
+else {
+    Console.WriteLine("не делится");
+}
