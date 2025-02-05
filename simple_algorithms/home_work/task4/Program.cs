0ce8f82735771eb6de4b09606b3b8691029ed878
Yeldos Baltabai <yeldosbaltabai@gmail.com>
2025-02-05 21:27:20 +0500

    fourth task of simple algoritms is solved

---
+++ simple_algorithms/home_work/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10) {
+    Console.Write(num);
+}
+else {
+    while (num > 0) {
+        int current_digit = num % 10;
+        num /= 10;
+        if (num > 0) {
+            Console.Write($"{current_digit}, ");
+        }
+        else{
+            Console.Write(current_digit);
+        }
+    }
+}
