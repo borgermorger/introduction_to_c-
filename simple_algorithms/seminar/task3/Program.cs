0a05098d44eff73649ecab9d8945250c72a4326a
Yeldos Baltabai <yeldosbaltabai@gmail.com>
2025-02-05 20:04:49 +0500

    theird task from simple algorithms seminar is solved

--- simple_algorithms/seminar/task3/Program.cs
+++ simple_algorithms/seminar/task3/Program.cs
@@ -7,3 +7,16 @@
 // 16, 8 => да
 // 4, 3 => нет, 1
 
+Console.Write("Введите первое положительное число: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе положительное число: ");
+int second_num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("является ли первое число кратным второму?");
+int remainder = first_num % second_num;
+if (remainder == 0) {
+    Console.WriteLine("да, является");
+}
+else {
+    Console.WriteLine($"нет, есть остаток равный: {remainder}");
+}
