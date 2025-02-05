df274dee5935b87c5a8752e924bb75c76ae1aa17
Yeldos Baltabai <yeldosbaltabai@gmail.com>
2025-02-05 21:17:47 +0500

    theird task of simple algorithms homewwork is solved

---
+++ simple_algorithms/home_work/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+
+Console.Write("Введите целое число в промежутке от 10 до 99 включительно: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num > 99 || num < 10) {
+    Console.Write("Некоррректный ввод. Введите целое число в промежутке от 10 до 99 включительно: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+int first_digit = num / 10;
+int second_digit = num % 10;
+
+if (first_digit >= second_digit) {
+    Console.WriteLine($"Наибольщая цифра равна: {first_digit}");
+}
+else {
+    Console.WriteLine($"Наибольщая цифра равна: {second_digit}");
+}
