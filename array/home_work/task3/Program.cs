b0545a19296e5c7a32d948321047e4993a101088
Yeldos Baltabai <yeldosbaltabai@gmail.com>
2025-02-09 14:23:10 +0500

    second and theird task of array homework is solved

---
+++ array/home_work/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задание 3: Поиск среднего значения массива
+// Описание: Задайте массив из 10 целых чисел. Найдите среднее значение элементов
+// массива.
+
+// Создаем объект рандомн
+Random rnd = new Random();
+
+// создаем массив и заполняем его рандомными цифрами от -10 до 10
+Console.WriteLine("Создается массив в десять целых чисел от -10 до 11");
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++){
+    array[i] = rnd.Next(-10, 11);
+}
+
+// выводим массив 
+foreach (int e in array) {
+    Console.Write($"{e} ");
+}
+Console.WriteLine("");
+
+double sum = 0;
+double average = 0;
+foreach (int e in array) {
+    sum += e;
+    average = sum / array.Length;
+}
+
+Console.WriteLine($"Среднее значение цифр массива равна: {average}");
