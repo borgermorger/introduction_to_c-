1a8e1f2fc6b64202ba50468cf39c37e0e788add4
Baltabai Yeldos <yeldosbaltabai@gmail.com>
2025-02-24 11:07:17 +0500

    array and string seminar's theird task is solved

---
+++ array_and_strings/seminar/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задание 3
+
+// Считать сторку с консоли, состоящую из латинских букв в нижнем 
+// регистре. Выяснить, сколько среди введённых букв гласных.
+
+// Пример
+
+// "Hello" => 2
+// "world" => 1
+
+int CountLowerCaseVowels(string text) {
+    string lowerCaseVowels = "eyuioa";
+    int count = 0;
+    foreach(char e in text) {
+        foreach(char i in lowerCaseVowels) {
+            if (e == i) {
+                count +=1;
+            }
+        }
+    }
+    return count; 
+}
+
+string str = "Hello world!";
+int result = CountLowerCaseVowels(str);
+Console.WriteLine($"В тексте: '{str}' - {result} маленьких гласных латинских букв.");
