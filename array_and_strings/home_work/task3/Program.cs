023c728853cc34f59c55f88e130734fc44655a41
Yeldos Baltabai <yeldosbaltabai@gmail.com>
2025-03-27 19:44:31 +0500

    solved all tasks from array and string homework

---
+++ array_and_strings/home_work/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+bool IsPolindrom(string text) {
+    bool res = true;
+    for(int i = 0; i < text.Length / 2; i++) {
+        if (text[i] == text[text.Length - i - 1]) {
+        }
+        else{
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
+
+Console.Write("Введите строку: ");
+string text = Console.ReadLine();
+
+if(IsPolindrom(text)){
+    Console.Write("Yaappiii is polindom");
+}
+else{
+    Console.Write("Ohh no it aint polindrom");
+}
---
+++ array_and_strings/home_work/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. В полученной строке 
+// слова должны быть также разделены пробелами.
+
+string ReversWords(string text){
+    string[] words = text.Split(" ");
+    Array.Reverse(words);
+    return string.Join(" ",words);
+}
+
+Console.Write("Введите строку: ");
+string text = Console.ReadLine();
+
+Console.WriteLine(ReversWords(text));
